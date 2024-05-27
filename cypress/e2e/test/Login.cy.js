662f99ab1a376660af24631f2cc38ccfd3e92b76
Monika <monikasingh.949@gmail.com>
2024-05-27 13:15:48 +1000

    updated the login test case

--- cypress/e2e/test/Login.cy.js
+++ cypress/e2e/test/Login.cy.js
@@ -1,5 +1,7 @@
 import { visitHomePage } from "../utils";
 import * as login from "../pages/login";
+import * as loginLocator from "../pages/objects/login.locators";
+import { loginAndSignUpHeader } from "../pages/objects/home.locators";
 
 describe("Login Functionality", () => {
   beforeEach(() => {
@@ -10,23 +12,25 @@ describe("Login Functionality", () => {
     // login with valid credenctials
     login.AuthenticateUser("rihef83861@godsigma.com", "passwordtest");
   });
-  // it("should show error message for invalid credentials", () => {
-  //   login.AuthenticateUser();
-  //   cy.contains("The login was not successful.").should("be.visible");
-  // cy.contains(
-  //   "The email address & password combination you have entered is incorrect.  Please try again or click the forgotten password link below to reset your password."
-  // ).should("be.visible");
-  // });
+  it("should show error message for invalid credentials", () => {
+    login.AuthenticateUser("rihef83861@godsigma.com", "passwordt");
+    cy.contains("The login was not successful.").should("be.visible");
+    cy.contains(
+      "Sorry we are experiencing some difficulties. Please try again."
+    ).should("be.visible");
+  });
 
-  // it("should show validation errors for empty fields", () => {
-  //   cy.get('button[type="submit"]').click();
-  //   cy.contains("Email address is required.").should("be.visible");
-  //   cy.contains("Password is required.").should("be.visible");
-  // });
-  // it("should show validation errors for invalid email address", () => {
-  //   cy.get("#signInForm-email").type("rrihef83861@godsigma");
-  //   cy.get("#signInForm-password").type("passwordtestt");
-  //   cy.get('button[type="submit"]').click();
-  //   cy.contains("The login was not successful.").should("be.visible");
-  // });
+  it("should show validation errors for empty fields", () => {
+    cy.get(loginAndSignUpHeader).click();
+    cy.get(loginLocator.submitButtonLocator).click();
+    cy.contains("Email address is required.").should("be.visible");
+    cy.contains("Password is required.").should("be.visible");
+  });
+  it("should show validation errors for invalid email address", () => {
+    login.AuthenticateUser("rrihef83861@godsigma", "passwordtestt");
+    // cy.get("#signInForm-email").type("rrihef83861@godsigma");
+    // cy.get("#signInForm-password").type("passwordtestt");
+    // cy.get('button[type="submit"]').click();
+    cy.contains("The login was not successful.").should("be.visible");
+  });
 });
